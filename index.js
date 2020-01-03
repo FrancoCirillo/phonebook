8c1a41aaf2a44166c24211de5338e14b32708844
Frilo <francocirillo3@gmail.com>
2020-01-03 11:17:52 -0300

    3.9 phonebook backend step9

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 const bodyParser = require('body-parser')
 
+app.use(cors())
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
 morgan.token('body',  (req, res) => JSON.stringify(req.body) )
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
