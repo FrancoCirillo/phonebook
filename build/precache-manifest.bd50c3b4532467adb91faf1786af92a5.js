80aed3150aebbada61b6b0772cdd5c4e2733ae01
Frilo <francocirillo3@gmail.com>
2020-01-03 12:07:25 -0300

    Add static build

---
+++ build/precache-manifest.bd50c3b4532467adb91faf1786af92a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1788c2a7568fc80de75442cd6ff12bf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bad775525af77306e90",
+    "url": "/static/css/main.251313f5.chunk.css"
+  },
+  {
+    "revision": "e3353dd6136d33895573",
+    "url": "/static/js/2.1ccff85f.chunk.js"
+  },
+  {
+    "revision": "8636da01741820916e773b0aaacc773d",
+    "url": "/static/js/2.1ccff85f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bad775525af77306e90",
+    "url": "/static/js/main.2a856e6d.chunk.js"
+  },
+  {
+    "revision": "f7fc8eba76c28cae35bb",
+    "url": "/static/js/runtime-main.a53a739b.js"
+  }
+]);
--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(bodyParser.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -42,7 +43,7 @@ app.get('/', (req, res) => {
     res.send('<h2>The Phonebook</h2>')
 })
 
-app.get('/api/persons', (req, res) => {
+app.get('/persons', (req, res) => {
     res.json(persons)
 })
 
@@ -53,14 +54,14 @@ app.get('/info', (req, res) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
     if (person) response.json(person)
     else response.status(404).end()
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/persons/:id', (request, response) => {
     const id = Number(request.params.id)
     persons = persons.filter(person => person.id !== id)
     response.status(204).end()
@@ -75,7 +76,7 @@ function getRandomId() {
 const infoMissing = body => body.name === "" || body.number === ""
 const reapetedName = body => persons.some(person => person.name === body.name)
 
-app.post('/api/persons', (request, response) => {
+app.post('/persons', (request, response) => {
     const body = request.body
     if (infoMissing(body)) {
         return response.status(400).json({
