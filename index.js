e2aee0f05f195ae92c402143b2b4735b9e7c9c61
Frilo <francocirillo3@gmail.com>
2020-01-04 15:40:18 -0300

    Exercises 3.12 to 3.14

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
 const bodyParser = require('body-parser')
@@ -11,40 +13,21 @@ app.use(bodyParser.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const PORT = 3001
+const PORT = process.env.PORT
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
-
-let persons = [
-    {
-        "name": "Arto Hellas",
-        "number": "040-123456",
-        "id": 1
-    },
-    {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-    },
-    {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
 
 app.get('/', (req, res) => {
     res.send('<h2>The Phonebook</h2>')
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(contacts => {
+        res.json(contacts.map(c => c.toJSON()))
+    })
+
 })
 
 app.get('/info', (req, res) => {
@@ -52,7 +35,6 @@ app.get('/info', (req, res) => {
     res.send(`<p>The phonebook has info from ${ammount} people</p>
     <p>${new Date()}</p>`)
 })
-
 
 app.get('/api/persons/:id', (request, response) => {
     const id = Number(request.params.id)
@@ -67,32 +49,19 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-function getRandomId() {
-    min = 0
-    max = 2147483647
-    return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const infoMissing = body => body.name === "" || body.number === ""
-const reapetedName = body => persons.some(person => person.name === body.name)
-
 app.post('/api/persons', (request, response) => {
     const body = request.body
-    if (infoMissing(body)) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-    if (reapetedName(body)) {
-        return response.status(409).json({
-            error: 'name already on the server'
-        })
-    }
-    const person = {
+
+
+    const person = new Person({
         name: body.name,
-        number: body.number,
-        id: getRandomId(),
-    }
+        number: body.number
+    })
+
+    person.save().then(response => {
+        console.log(`Added ${person.name}, number ${person.number} to the phonebook`);
+        mongoose.connection.close();
+    })
 
     persons = persons.concat(person)
     response.json(person)
---
+++ mongo.js
@@ -0,0 +1,41 @@
+/*Only for testing*/
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://newUser:${password}@clusterphone-7eac0.mongodb.net/the-phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length >= 4) {
+    const person = new Person({
+        name: process.argv[3], number: process.argv[4]
+    })
+
+    person.save().then(response => {
+        console.log(`Added ${person.name}, number ${person.number} to the phonebook`);
+        mongoose.connection.close();
+    })
+}
+
+if (process.argv.length < 4) {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person)
+        })
+        mongoose.connection.close()
+    })
+}
